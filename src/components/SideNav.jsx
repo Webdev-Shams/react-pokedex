62d4085a89c9be4308233d0cce935f3cb4a6a506
Shams Saif <96794669+Webdev-Shams@users.noreply.github.com>
2025-09-30 10:49:01 +0600

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react"
 import { first151Pokemon, getFullPokedexNumber } from "../utils"
 
 export function SideNav (props) {
-    const {selectedPokemon, setSelectedPokemon} = props
+    const {selectedPokemon, setSelectedPokemon, handleToggleMenu, showSideMenu} = props
+    const [searchValue, setSearchValue] = useState ('')
+    const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+        // if(toString(getFullPokedexNumber(eleIndex)).includes(searchValue)) {return true}
+        if(getFullPokedexNumber(eleIndex).includes(searchValue)) {return true}
+
+        if(ele.toLowerCase().includes(searchValue.toLowerCase())) {return true}
+
+        return false
+    })
+
     return (
-        <nav>
-            <div className={"header"}>
+        <nav className={' ' + (!showSideMenu ? " open" : '')}>
+            <div className={"header" + (!showSideMenu ? " open" : '')}>
+                <button onClick={handleToggleMenu} className="open-nav-button">
+                    <i className="fa-solid fa-chevron-left"></i>
+                </button>
                 <h1 className="text-gradient">
                     Pokedex
+                    
                 </h1>
             </div> 
-            <input />
+            <input value={searchValue} onChange={(e) => {setSearchValue(e.target.value)}} />
+
             {
-                first151Pokemon.map((pokemon, pokemonIndex) => {
+                filteredPokemon.map((pokemon, pokemonIndex) => {
+                    const truePokedexNumber = first151Pokemon.indexOf(pokemon)
                     return (
-                        <button className={'nav-card'} key={pokemonIndex}>
-                            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        <button onClick={() => {setSelectedPokemon(truePokedexNumber)}} className={'nav-card ' + (pokemonIndex === selectedPokemon ? 'nav-card-selected' : ' ')} key={pokemonIndex}>
+                            <p>{getFullPokedexNumber(truePokedexNumber)}</p>
                             {pokemon}
                         </button>
                     )
